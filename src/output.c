c2f4752f57a5ca5cddb6d5ea6c1d46966a1f35ba
Willem Elbers <willem.elbers@gmail.com>
2020-03-20 03:28:43 +0000

    Write attributes to output file

--- src/output.c
+++ src/output.c
@@ -22,7 +22,7 @@
 int write_perturb(struct perturb_data *data, struct params *pars,
                   struct units *us, char *fname) {
     /* The memory for the transfer functions is located here */
-    hid_t h_file, h_grp, h_data, h_err;
+    hid_t h_file, h_grp, h_data, h_err, h_space, h_attr;
 
     /* Open file */
     h_file = H5Fcreate(fname, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
@@ -34,22 +34,49 @@ int write_perturb(struct perturb_data *data, struct params *pars,
     h_grp = H5Gcreate(h_file, "/Header", H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
     if (h_grp < 0) printf("Error while creating file header\n");
 
-      // /* Print the relevant information and print status */
-      // io_write_attribute(h_grp, "k_size", INT, &data->k_size, 1); //H5T_NATIVE_INT
-      // io_write_attribute(h_grp, "tau_size", INT, &data->tau_size, 1);
-      // io_write_attribute(h_grp, "n_functions", INT, &data->n_functions, 1);
-      //
-      // /* Write unit system for this data set */
-      // io_write_attribute_d(h_grp, "Unit mass in cgs (U_M)",
-      //                      units_get_base_unit(us, UNIT_MASS));
-      // io_write_attribute_d(h_grp, "Unit length in cgs (U_L)",
-      //                      units_get_base_unit(us, UNIT_LENGTH));
-      // io_write_attribute_d(h_grp, "Unit time in cgs (U_t)",
-      //                      units_get_base_unit(us, UNIT_TIME));
-      // io_write_attribute_d(h_grp, "Unit current in cgs (U_I)",
-      //                      units_get_base_unit(us, UNIT_CURRENT));
-      // io_write_attribute_d(h_grp, "Unit temperature in cgs (U_T)",
-      //                      units_get_base_unit(us, UNIT_TEMPERATURE));
+    /* We will write attributes, each consisting of a single scalar number */
+    int ndims = 1;
+    hsize_t dim[1] = {1};
+
+    /* Create dataspace */
+    h_space = H5Screate(H5S_SIMPLE);
+    H5Sset_extent_simple(h_space, ndims, dim, NULL);
+
+    /* Write the size of the perturbation (along the k-dimension) */
+    h_attr = H5Acreate1(h_grp, "k_size", H5T_NATIVE_INT, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_INT, &data->k_size);
+    H5Aclose(h_attr);
+
+    /* Write the size of the perturbation (along the tau-direction) */
+    h_attr = H5Acreate1(h_grp, "tau_size", H5T_NATIVE_INT, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_INT, &data->tau_size);
+    H5Aclose(h_attr);
+
+    /* Write the number of transfer functions */
+    h_attr = H5Acreate1(h_grp, "n_functions", H5T_NATIVE_INT, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_INT, &data->n_functions);
+    H5Aclose(h_attr);
+
+    /* Determine the units used */
+    double unit_mass_cgs = us->UnitMassKilogram * 1000;
+    double unit_length_cgs = us->UnitLengthMetres * 100;
+    double unit_time_cgs = us->UnitTimeSeconds;
+
+    /* Write the internal unit system */
+    h_attr = H5Acreate1(h_grp, "Unit mass in cgs (U_M)", H5T_NATIVE_DOUBLE, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_DOUBLE, &unit_mass_cgs);
+    H5Aclose(h_attr);
+
+    h_attr = H5Acreate1(h_grp, "Unit length in cgs (U_L)", H5T_NATIVE_DOUBLE, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_DOUBLE, &unit_length_cgs);
+    H5Aclose(h_attr);
+
+    h_attr = H5Acreate1(h_grp, "Unit time in cgs (U_t)", H5T_NATIVE_DOUBLE, h_space, H5P_DEFAULT);
+    h_err = H5Awrite(h_attr, H5T_NATIVE_DOUBLE, &unit_time_cgs);
+    H5Aclose(h_attr);
+
+    /* Done with the single scalar dataspace */
+    H5Sclose(h_space);
 
     /* Close header */
     H5Gclose(h_grp);
@@ -59,7 +86,7 @@ int write_perturb(struct perturb_data *data, struct params *pars,
     if (h_grp < 0) printf("Error while creating perturbation group\n");
 
     /* Create data space */
-    const hid_t h_space = H5Screate(H5S_SIMPLE);
+    h_space = H5Screate(H5S_SIMPLE);
     if (h_space < 0) printf("Error while creating data space.\n");
 
     /* Set the extent of the data */
